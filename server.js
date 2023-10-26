46f94ff7285bbd35a35908f80944b3af65e355cc
Andrii Chekurda <andrii_chekurda@epam.com>
2023-10-26 20:12:37 +0300

    upload API [done] - added uploads endpoint

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const multer = require('multer');
+const cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+})
+  
+const upload = multer({ storage: storage })
+
+app.post("/api", upload.array("files"), (req, res) => {
+
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "File(s) uploaded successfully" });
+
+});
+
+app.listen(5000, function(){
+    console.log("Server running on port 5000");
+});
